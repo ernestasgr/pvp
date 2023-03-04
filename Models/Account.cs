439de8288325a78588fc49bb7957da0133734159
ernestasgr <ernestasgr02@gmail.com>
2023-03-04 13:34:09 +0200

    remote db + account pavyzdys

---
+++ Models/Account.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+	public class Account
+	{
+		[Key]
+		public int IDAccount { get; set; }
+
+		[Required]
+		public string? Nickname { get; set; }
+
+		[Required]
+		public string? Email { get; set; }
+
+		[Required]
+		public string? Hash { get; set; }
+
+        [Required]
+		public string? Role { get; set; }
+	}
+}
