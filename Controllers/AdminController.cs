b791c9e85ba352046e6ffb0ca6e18a32f4e1889b
vytcka <vytas.sa1@gmail.com>
2023-03-07 01:51:44 +0200

    Added users page with view of current users

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,18 +2,33 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using test.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace test.Controllers
 {
 	public class AdminController : Controller
     {
-        public IActionResult Index()
+
+        private readonly AppDbContext _context;
+
+        public AdminController(AppDbContext context)
         {
-            return View();
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult Users()
+        public async Task<IActionResult> Users(string searchId)
+        {
+            var users = _context.Accounts.ToList();
+            if (!string.IsNullOrEmpty(searchId))
+            {
+                users = users.Where(u => u.IDAccount.ToString().Contains(searchId)).ToList();
+            }
+
+            return View(users);
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
